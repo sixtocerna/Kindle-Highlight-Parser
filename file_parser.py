636991cb7d867735a12443ad2c3cdf418ae2ec89
sixtocerna <107313465+sixtocerna@users.noreply.github.com>
2024-03-25 16:23:56 -0600

    books names fixed
    Standarized the format for documents titles for easier parsing

--- file_parser.py
+++ file_parser.py
@@ -10,13 +10,49 @@ class ParsingError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
 
+class PatternError(Exception):
+    """
+    Exception raised when the text format does not match the expected pattern.
+    """
 
-def validate_single_match(
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+def check_text_format(text_to_check:str, expected_pattern:str) -> None:
+    """
+    Checks if a text matches the expected pattern using regular expressions.
+
+    Args:
+        text_to_check (str): The text to be checked.
+        expected_pattern (str): The regular expression pattern representing the expected format.
+
+    Raises:
+        PatternError: If the text does not match the expected pattern.
+    """
+
+    if not re.match(expected_pattern, text_to_check):
+        raise PatternError(f'{text_to_check} does not match format expected : {expected_pattern}')
+
+def check_single_match(
         matches:List[T], 
         exactly_one:bool = True,
         msg_more_than_one_match: str = '', 
         msg_no_matches:str = '') -> T:
+    """
+    Validates a list of matches, ensuring there is only one match or raising exceptions accordingly.
 
+    Args:
+        matches (List[T]): A list of matches to validate.
+        exactly_one (bool, optional): If True, exactly one match is expected. Defaults to True.
+        msg_more_than_one_match (str, optional): Error message when more than one match is found. Defaults to ''.
+        msg_no_matches (str, optional): Error message when no matches are found. Defaults to ''.
+
+    Returns:
+        T: The single match if exactly one match is found.
+
+    Raises:
+        ValueError: If no matches are found or if more than one match is found.
+    """
     if len(matches)==0 :
         raise ValueError(msg_no_matches)
     elif len(matches)==1:
@@ -27,8 +63,28 @@ def validate_single_match(
 
 
 class Highlight():
+    """
+    Represents a highlighted section from a document.
+
+    Attributes:
+        document_name (str): The name of the document.
+        date (datetime): The date the highlight was made.
+        pages (Tuple[int]): A tuple representing the start and end pages of the highlight.
+        content (str): The content of the highlight.
+        author (str, optional): The author of the document. Defaults to None.
+    """
 
     def __init__(self, document_name:str, date:datetime, pages:Tuple[int], content:str, author=None) -> None:
+        """
+        Initializes a Highlight object.
+
+        Args:
+            document_name (str): The name of the document.
+            date (datetime): The date the highlight was made.
+            pages (Tuple[int]): A tuple representing the start and end pages of the highlight.
+            content (str): The content of the highlight.
+            author (str, optional): The author of the document. Defaults to None.
+        """
         
         self.document_name = document_name
         self.date = date
@@ -41,57 +97,55 @@ class Highlight():
         {self.content}
         """
 
-    def _parse_title_and_author(text:str, ignore_missing_author = True, ignore_missing_extension = True) -> str:
+    def _parse_title_and_author(text:str) -> str:
+        """
+        Parses the title and author from the given text.
 
-        pattern = r'\([^)]*\)'
-        # Remove text within parentheses using regex
-        clean_text = re.sub(pattern, '', text)
+        Expects the following formats :
+            - 'Title by author.extension'
 
-        document_name, *extension = clean_text.split('.')
+        Args:
+            text (str): The text containing the title and author information.
+            ignore_missing_author (bool, optional): Whether to ignore missing author information. Defaults to True.
+            ignore_missing_extension (bool, optional): Whether to ignore missing file extension. Defaults to True.
 
-        if len(extension)>1:
-            raise ParsingError(("Can't parse title and author : text contains " 
-                              "multiple times the sign '.' "))
-        elif len(extension)==0:
-            if ignore_missing_extension:
+        Returns:
+            Tuple[str]: The parsed title and author.
 
-                title = document_name
-                author = None
+        Raises:
+            ParsingError: If the are multiple dots, 'by' or '(' inside the text.
+        """
 
-                return title, author
-            else:
-                raise ParsingError
+        pattern = r'^.* by .*$'
 
-        title, *rest = document_name.split('by')
+        check_text_format(text_to_check=text, expected_pattern=pattern)
 
-        if len(rest)>1:
-            raise ParsingError(("Can't parse title and author : text contains " 
-                              "multiple times the word 'by'."))
-        elif len(rest)==0:
-            if ignore_missing_author:
-                title = document_name
-                author = None
+        content, *extension = text.split('.')
 
-                return title, author
-            else:
-                raise ParsingError()
-        
-        title = title.strip()
-        
-        author, *remaing_text = rest[0].split('(')
+        title, author = content.split('by')
 
-        if len(remaing_text)>1:
-            raise ParsingError(("Can't parse title and author : text contains" 
-                              "multiple times the sign '('."))
-        
         return title, author
 
+
+
     def _parse_date(text:str) -> str:
+        """
+        Parses the date from the given text.
+
+        Args:
+            text (str): The text containing the date information.
+
+        Returns:
+            str: The parsed date.
+
+        Raises:
+            ParsingError: If no date in the format 'Month date year' is found within the text
+        """
 
         date_pattern = r"\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}\b"
         matches = re.findall(date_pattern, text)
 
-        date_string = validate_single_match(matches=matches, 
+        date_string = check_single_match(matches=matches, 
                                        msg_no_matches='Text must contain a date',
                                        msg_more_than_one_match='Text can only contain a single date')
 
@@ -99,19 +153,31 @@ class Highlight():
 
 
     def _parse_pages(text:str) -> Tuple[int]:
+        """
+        Parses the pages from the given text.
+
+        Args:
+            text (str): The text containing the page information.
+
+        Returns:
+            Tuple[int]: A tuple representing the start and end pages.
+
+        Raises:
+            ParsingError: If no pages in the format 'digit' or 'digits-digits' is found
+        """
         pattern = r'(\d+-\d+)'
         matches = re.findall(pattern, text)
 
         try: 
 
-            pages = validate_single_match(matches=matches, msg_more_than_one_match='Too many pages to parse', msg_no_matches='No pages found')
+            pages = check_single_match(matches=matches, msg_more_than_one_match='Too many pages to parse', msg_no_matches='No pages found')
             start_page, end_page = pages.split('-')
 
         except ValueError:
 
             pattern = r'page (\d+)'
             matches = re.findall(pattern, text)
-            page = validate_single_match(matches=matches, msg_more_than_one_match='Too many pages to parse', msg_no_matches='No pages found')
+            page = check_single_match(matches=matches, msg_more_than_one_match='Too many pages to parse', msg_no_matches='No pages found')
             start_page, end_page = page, page
 
         return int(start_page), int(end_page)
@@ -119,61 +185,63 @@ class Highlight():
     @classmethod
     def from_text(cls, raw_text:str):
         """
-        Magic Words (Jonah Berger) (Z-Library)  
-        - Your Highlight on page 141-141 | Added on Thursday, March 21, 2024 4:20:33 AM
+        Creates a Highlight object from raw text.
 
-        again—and mixing up moments—leveraging emotional volatility —can help turn any story into a great one
+        Args:
+            raw_text (str): The raw text containing highlight details.
+
+        Returns:
+            Highlight: A Highlight object created from the raw text.
         """
+
         raw_text = raw_text.strip()
         raw_document_name, details, *content = raw_text.splitlines()
-        #print('DETAILS : ', details)
 
-        try : 
-            date = cls._parse_date(details)
-        except:
-            print('Error in date parsing', details)
-            raise Exception
+
+        date = cls._parse_date(details)
         title, author = cls._parse_title_and_author(raw_document_name)
         pages = cls._parse_pages(details)
 
         return cls(document_name =title, date=date, pages=pages, content = ''.join(content), author=author)
+    
+    @staticmethod
+    def read_file(filename:str = 'My Clippings.txt') -> str:
+        """
+        Reads the contents of a text file and returns the text.
 
+        Args:
+            filename (str, optional): The name of the file to read. Should be in the same directory. Defaults to 'My Clippings.txt'.
 
-def read_file(filename:str = 'My Clippings.txt') -> str:
-    """
-    Returns the text contains in file
+        Returns:
+            str: The text contained in the file.
 
-    Parameters : 
-    - filename (str): Name of the file. Should be in the same directory
-    Returns
-    - str
-    """
+        """
 
-    with open(filename, 'r') as file:
+        with open(filename, 'r') as file:
 
-        text = file.read()
-        
-    return text
+            text = file.read()
+            
+        return text
 
+    @staticmethod
+    def parse_highlights_file() -> List[]:
+        """
+        Returns the list of highlights
+        """
 
-def split_into_highlights(file_content:str) -> List[str]:
-    """
-    Returns the list of highlights
+        file_content = Highlight.read_file('My Clippings updated.txt')
 
-    """
+        raw_highlights =  file_content.split('==========')
 
-    highlights_data =  file_content.split('==========')
+        output = []
 
-    for data in highlights_data:
+        for highlight in raw_highlights:
 
-        print(data)
+            if len(highlight.strip())==0:
+                continue
 
-        h  = Highlight.from_text(data)
+            highlight_processed  = Highlight.from_text(highlight)
 
-        print(str(h))
-        print('='*50)
-        print()
-        print()
+            output.append(highlight_processed)
 
-
-print(split_into_highlights(read_file()))
+        return output
